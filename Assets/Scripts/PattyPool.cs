63392c5efad8b354b926539f53de8532bb974d35
Jose Nicolas Santander <josess.nicolas@hotmail.com>
2020-04-05 00:06:15 -0500

    Arms

---
+++ Assets/Scripts/PattyPool.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class PattyPool : MonoBehaviour
+{
+    [SerializeField] private Queue<GameObject> pattyPool = new Queue<GameObject>();
+    [SerializeField] private GameObject patty;
+    [SerializeField] private int poolSize;
+
+    private void Start()
+    {
+        for (int i = 0; i < poolSize; i++)
+        {
+            GameObject spawnedPatty = Instantiate(patty, new Vector3(100,100,100),Quaternion.identity);
+            spawnedPatty.transform.SetParent(transform);
+            pattyPool.Enqueue(spawnedPatty);
+            spawnedPatty.SetActive(false);
+        }
+        Debug.Log(pattyPool.Count);   
+    }
+
+    public void SpawnPatty(Vector2 position)
+    {
+        GameObject pattyToSpawn = pattyPool.Dequeue();
+        
+        pattyToSpawn.SetActive(true);
+        pattyToSpawn.transform.position = position;
+        
+        pattyPool.Enqueue(pattyToSpawn);
+    }
+}
