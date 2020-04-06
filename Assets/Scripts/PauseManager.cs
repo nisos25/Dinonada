64cb43513caec770c5dc92cdfcfaeae01eb1c219
Daniel <jdanielsancheza@gmail.com>
2020-04-06 02:19:10 -0500

    Agregado PauseManager

---
+++ Assets/Scripts/PauseManager.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseManager : MonoBehaviour
+{
+    [SerializeField] private Canvas pauseCanvas;
+    [SerializeField] private GameObject pauseImage;
+    [SerializeField] private GameObject settingsImage;
+	[SerializeField] CanvasGroup CanvasGroup;
+    private bool paused;
+
+	void Start()
+	{
+		pauseCanvas.enabled =false;
+		CanvasGroup.interactable = false;
+	}
+
+	private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            paused = true;
+            Pause();
+        }
+    }
+
+    public void Pause()
+    {
+        pauseCanvas.enabled = !pauseCanvas.enabled;
+		CanvasGroup.interactable = pauseCanvas.enabled;
+
+		Time.timeScale = Time.timeScale == 1 ? 0 : 1;
+    }
+
+    public void LoadSettings()
+    {
+        pauseImage.SetActive(false);
+        settingsImage.SetActive(true);
+    }
+
+    public void LoadPause()
+    {
+        pauseImage.SetActive(true);
+        settingsImage.SetActive(false);
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Menu");
+    }
+    
+}
