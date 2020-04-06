7022c250e563370d66efa4a96595a53bbcc9f9a8
Daniel <jdanielsancheza@gmail.com>
2020-04-06 01:14:28 -0500

    DinoPipol como prefab opciones y diversión garantizada

--- Assets/Scripts/PeopleObject.cs
+++ Assets/Scripts/PeopleObject.cs
@@ -13,18 +13,39 @@ public class PeopleObject : MonoBehaviour
     private int timepoInteresado;
     [SerializeField]
     private int cantidadEmpanadas;
+    [SerializeField]
+    private GameObject pensamiento;
+
+    public bool special;
 
     private Rigidbody2D rb;
 
     void Start()
     {
+        Random.InitState(System.DateTime.Now.Millisecond);
+
+        pensamiento = transform.GetChild(0).gameObject;
         facingRight = transform.position.x < 0 ? true : false;
+        GetComponent<SpriteRenderer>().flipX = facingRight;
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0;
+
+        if(special)
+        {
+            cantidadEmpanadas = 5;
+            speed = 4;
+            ///sonido especial
+        }
+        else
+        {
+            RandomParameters();
+            StartCoroutine(Timer());
+        } 
+
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         rb.velocity = facingRight ? Vector2.right * speed : Vector2.left * speed;
     }
@@ -38,8 +59,58 @@ public class PeopleObject : MonoBehaviour
     {
         if(collision.CompareTag("Empanada"))
         {
-            ///compra las empanadas
+            if(cantidadEmpanadas>0)
+            {
+                Destroy(collision.gameObject);
+                ReduceEmpanadasQuantity();
+                ///Vender
+            }
         }
+    }
+
+    void ReduceEmpanadasQuantity()
+    {
+        cantidadEmpanadas--;
+        ///sonido moneda
+        if(cantidadEmpanadas <= 0)
+        {
+            ///Sonido caching
+            DeactivateInterestInEmpanadas();
+        }
+    }
+
+    void RandomParameters()
+    {
+        cantidadEmpanadas = Random.Range(0,3);
+        //speed += Random.Range(-1,1);
+        if(cantidadEmpanadas > 0)
+        {
+            pensamiento.SetActive(true);
+
+            timepoInteresado = Random.Range(4,10);
+        }
+        else
+        {
+            DeactivateInterestInEmpanadas();
+        }
+
+    }
+
+    IEnumerator Timer()
+    {
+        yield return new WaitForSeconds(2);
+        timepoInteresado--;
+        if(timepoInteresado <= 0)
+        {
+            DeactivateInterestInEmpanadas();
+            StopCoroutine(Timer());
+        }
+    }
+
+    void DeactivateInterestInEmpanadas()
+    {
+        GetComponent<CapsuleCollider2D>().enabled = false;
+        transform.GetChild(0).gameObject.SetActive(false);
     }
 
 }
